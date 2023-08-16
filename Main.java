9d0bf61b5b478caa169e4f749cb97d58b351aadd
SmuziGudi <113757462+SmuziGudi@users.noreply.github.com>
2023-08-16 15:35:41 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -32,21 +32,27 @@ public class Main {
         String stable00 = blacks[0];
         String stable01 = blacks[1];
         String string03 = stable01.trim();
-        number1 = romanToNumber(stable00);
-        number2 = romanToNumber(string03);
-        if (number1 < 0 && number2 < 0) {
-            result = 0;
-        } else {
-            result = calculated(number1, number2, operation);
-            System.out.println("Output: ");
-            try{
-                String resultRoman = convertNumToRoman(result);
-                System.out.println(resultRoman);
-                return;
-            } catch (Exception e) {
-                throw new RuntimeException("Римское число не может быть отрицательным");
+            number1 = romanToNumber(stable00);
+            number2 = romanToNumber(string03);
+            if (number1 != 0 && number2 != 0){
+                try {
+                    if (number1 < 0 && number2 < 0) {
+                        result = 0;
+                    } else {
+                        result = calculated(number1, number2, operation);
+                        System.out.println("Output: ");
+                        try{
+                            String resultRoman = convertNumToRoman(result);
+                            System.out.println(resultRoman);
+                            return;
+                        } catch (Exception e) {
+                            throw new RuntimeException("Римское число не может быть отрицательным");
+                        }
+                    }
+                } catch (RuntimeException e) {
+                    throw new RuntimeException("Калькулятор может работать только с 2 римскими или с 2 арабскими числами");
+                }
             }
-        }
             number1 = Integer.parseInt(stable00);
             number2 = Integer.parseInt(string03);
             if (number1 > 10 || number2 > 10){
@@ -57,7 +63,13 @@ public class Main {
     }
 
     private static String convertNumToRoman (int numArabian) {
-        String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"
+        String[] roman = {"null","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
         };
         final String s = roman[numArabian];
         return s;
@@ -65,32 +77,30 @@ public class Main {
 
 
     private static int romanToNumber (String roman) {
-        try {
-            if (roman.equals("I")) {
-                return 1;
-            } else if (roman.equals("II")) {
-                return 2;
-            } else if (roman.equals("III")) {
-                return 3;
-            } else if (roman.equals("IV")) {
-                return 4;
-            } else if (roman.equals("V")) {
-                return 5;
-            } else if (roman.equals("VI")) {
-                return 6;
-            } else if (roman.equals("VII")) {
-                return 7;
-            } else if (roman.equals("VIII")) {
-                return 8;
-            } else if (roman.equals("IX")) {
-                return 9;
-            } else if (roman.equals("X")) {
-                return 10;
+            switch (roman) {
+                case "I":
+                    return 1;
+                case "II":
+                    return 2;
+                case "III":
+                    return 3;
+                case "IV":
+                    return 4;
+                case "V":
+                    return 5;
+                case "VI":
+                    return 6;
+                case "VII":
+                    return 7;
+                case "VIII":
+                    return 8;
+                case "IX":
+                    return 9;
+                case "X":
+                    return 10;
+                default:
+                    return 0;
             }
-        } catch (InputMismatchException e) {
-            throw new InputMismatchException("Неверный формат данных");
-        }
-        throw new InputMismatchException("Калькулятор может работать только с 2 римскими или с 2 арабскими числами");
     }
 
     public static int calculated (int num1, int num2, char op) {
