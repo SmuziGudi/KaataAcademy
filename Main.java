d76f82b8d82d2faaa19b2d2b0e952ac237adab16
SmuziGudi <113757462+SmuziGudi@users.noreply.github.com>
2023-08-16 12:01:23 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,129 +1,123 @@
-import java.util.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
-        Integer[] arabic = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        String[] roman = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "XI", "X"};
+    static Scanner scanner = new Scanner(System.in);
+    static int number1, number2;
+    static char operation;
+    static int result;
 
-        System.out.println("input:");
-        Scanner sc = new Scanner(System.in);
-        int a = 0;
-        int b = 0;
-        String op = "";
-        do {
-            String input = sc.next();
-
-            String[] strings = input.split("\\W");
-            String[] operator = input.split("\\w");
-            try {
-                boolean flag = false;
-                for (int i = 0; i < roman.length; i++) {
-                    if (roman[i].equals(strings[0]) || roman[i].equals(strings[1])) {
-                        flag = true;
-                    }
-                }
-                    
-                    if (flag) {
-                        a = romanToNumber(strings[0]);
-                        b = romanToNumber(strings[1]);
-                    } else {
-                        a = Integer.parseInt(strings[0]);
-                        b = Integer.parseInt(strings[1]);
-                    }
-                    op = operator[operator.length - 1];
-
-                if (operation(a, b, op) < 0 || operator.length > 1){
-                    throw new RuntimeException();
-                }
-                if(flag){
-                    String result = numberToRoman(operation(a, b, op));
-                    System.out.println("output: " + result);
-                 }
-                else if (a <= 10 && b <= 10) {
-                    if (operation(a, b, op) >= 0) {
-                        System.out.println("output: " + operation(a, b, op));
-                    }
-                }
-            } catch (NumberFormatException e) {
-                throw new RuntimeException(e);
+    public static void main (String[] args) throws Exception {
+        System.out.println("Input: ");
+        String userInput = scanner.nextLine();
+        char[] under_char = new char[10];
+        for (int i = 0; i < userInput.length(); i++) {
+            under_char[i] = userInput.charAt(i);
+            if (under_char[i] == '+') {
+                operation = '+';
             }
-        } while (true);
-    }
-
-    public static int romanToNumber(String strings) {
-        if (strings.equals("I")) return 1;
-        if (strings.equals("II")) return 2;
-        if (strings.equals("III")) return 3;
-        if (strings.equals("IV")) return 4;
-        if (strings.equals("V")) return 5;
-        if (strings.equals("VI")) return 6;
-        if (strings.equals("VII")) return 7;
-        if (strings.equals("VIII")) return 8;
-        if (strings.equals("IX")) return 9;
-        if (strings.equals("X")) return 10;
-        return 0;
-    }
-    private static String numberToRoman(int result) {
-        switch (result) {
-            case 1:
-                return "I";
-            case 2:
-                return "II";
-            case 3:
-                return "III";
-            case 4:
-                return "IV";
-            case 5:
-                return "V";
-            case 6:
-                return "VI";
-            case 7:
-                return "VII";
-            case 8:
-                return "VIII";
-            case 9:
-                return "IX";
-            case 10:
-                return "X";
-            case 11:
-                return "XI";
-            case 12:
-                return "XII";
-            case 13:
-                return "XIII";
-            case 14:
-                return "XIV";
-            case 15:
-                return "XV";
-            case 16:
-                return "XVI";
-            case 17:
-                return "XVII";
-            case 18:
-                return "XVIII";
-            case 19:
-                return "XIX";
-            case 20:
-                return "XX";
-
-            default:
-                return null;
+            if (under_char[i] == '-') {
+                operation = '-';
+            }
+            if (under_char[i] == '*') {
+                operation = '*';
+            }
+            if (under_char[i] == '/') {
+                operation = '/';
+            }
         }
+        String under_charString = String.valueOf(under_char);
+        String[] blacks = under_charString.split("[+-/*]");
+        String stable00 = blacks[0];
+        String stable01 = blacks[1];
+        String string03 = stable01.trim();
+        number1 = romanToNumber(stable00);
+        number2 = romanToNumber(string03);
+        if (number1 < 0 && number2 < 0) {
+            result = 0;
+        } else {
+            result = calculated(number1, number2, operation);
+            System.out.println("Output: ");
+            try{
+                String resultRoman = convertNumToRoman(result);
+                System.out.println(resultRoman);
+                return;
+            } catch (Exception e) {
+                throw new RuntimeException("Римское число не может быть отрицательным");
+            }
+        }
+            number1 = Integer.parseInt(stable00);
+            number2 = Integer.parseInt(string03);
+            if (number1 > 10 || number2 > 10){
+            throw new Exception("Введи 2 числа в диапозоне от 0 до 10");
+            }
+            result = calculated(number1, number2, operation);
+            System.out.println("Output: \n"+result);
     }
 
-    private static int operation(int a, int b, String op) {
+    private static String convertNumToRoman (int numArabian) {
+        String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"
+        };
+        final String s = roman[numArabian];
+        return s;
+    }
+
+
+    private static int romanToNumber (String roman) {
+        try {
+            if (roman.equals("I")) {
+                return 1;
+            } else if (roman.equals("II")) {
+                return 2;
+            } else if (roman.equals("III")) {
+                return 3;
+            } else if (roman.equals("IV")) {
+                return 4;
+            } else if (roman.equals("V")) {
+                return 5;
+            } else if (roman.equals("VI")) {
+                return 6;
+            } else if (roman.equals("VII")) {
+                return 7;
+            } else if (roman.equals("VIII")) {
+                return 8;
+            } else if (roman.equals("IX")) {
+                return 9;
+            } else if (roman.equals("X")) {
+                return 10;
+            }
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Неверный формат данных");
+        }
+        throw new InputMismatchException("Калькулятор может работать только с 2 римскими или с 2 арабскими числами");
+    }
+
+    public static int calculated (int num1, int num2, char op) {
+        int result = 0;
         switch (op) {
-            case "*":
-                return a * b;
-            case "+":
-                return a + b;
-            case "-":
-                return a - b;
-            case "/":
-                return a / b;
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                try {
+                    result = num1 / num2;
+                } catch (ArithmeticException | InputMismatchException e) {
+                    System.out.println("Exception : " + e);
+                    System.out.println("Попробуй сначала, введи 2 натуральных числа > 0");
+
+                    break;
+                }
+                break;
             default:
-                return 0;
+                throw new IllegalArgumentException("Не верный знак операции");
         }
+        return result;
     }
 }
